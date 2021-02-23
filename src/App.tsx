24a857e7280da1a9cbd1b0c218273306525ab633
WINGLYst4rk <wingly205@gmail.com>
2021-02-22 22:09:35 -0300

    start the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { Button } from './components/Button'
+
 function App() {
   return (
-    <div>
-      <Button color="red">
-        Botão 1
-      </Button>
-      <Button color="blue">
-        Botão 2
-      </Button>
-      <Button color="green">
-        Botão 3
-      </Button>
-    </div>
+    
   );
 }
 
